f232d315c8b1d137147a340bb1f9e1ffca6041a3
randallreedjr <randallreedjr@gmail.com>
2014-06-13 11:17:35 -0400

    Make program loop, add some input validation

--- math24cli.rb
+++ math24cli.rb
@@ -1,7 +1,30 @@
 require_relative 'math24.rb'
 
-numbers = [6,6,8,11]
-operators = ["+", "-", "*", "/"]
-solver = Math24.new(operators)
-solver.numbers = numbers
-puts solver.solve()
+def run()
+  puts "Welcome to Math 24 solver! Please enter your numbers to see solution"
+  command = ""
+  operators = ["+", "-", "*", "/"]
+  solver = Math24.new(operators)
+  command = gets.chomp
+  while command.downcase != "exit"
+    number_entry = command
+    if number_entry.include?(",")
+      number_entry.gsub!(" ", "")
+      numbers = number_entry.split(",")
+    else
+      number_entry.squeeze(" ")
+      numbers = number_entry.split(" ")
+    end
+    if numbers.size == 4
+      solver.numbers = numbers
+      puts solver.solve()
+    else
+      puts "Please enter exactly four numbers"
+    end
+
+    command = gets.chomp
+  end
+
+end
+
+run()
