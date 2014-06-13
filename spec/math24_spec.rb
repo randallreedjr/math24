9fd4264d6b8d39f7df3dbea9be495de2eb3cc6b2
randallreedjr <randallreedjr@gmail.com>
2014-06-13 12:45:42 -0400

    Creates two basic tests

--- spec/math24_spec.rb
+++ spec/math24_spec.rb
@@ -1,5 +1,16 @@
-require_relaitve "../config/environment.rb"
+require_relative "../config/environment.rb"
 
-describe Math24 do
-  
+describe Math24, "#initialize" do
+  it "sets default operators" do
+    math24 = Math24.new()
+    math24.operators == ["+", "-", "*", "/"]
+  end
+end
+
+describe Math24, "solve" do
+  it "solave a simple addition example" do
+    math24 = Math24.new()
+    math24.numbers = ["6", "6", "6", "6"]
+    expect(math24.solve).to eq("6 + 6 + 6 + 6 = 24")
+  end
 end
