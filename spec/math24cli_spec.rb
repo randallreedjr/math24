bf83138e7d88f1f565bf23cdb8cbe96d37583e2c
randallreedjr <randallreedjr@gmail.com>
2014-06-13 15:09:12 -0400

    Test for exit

--- spec/math24cli_spec.rb
+++ spec/math24cli_spec.rb
@@ -1,14 +1,25 @@
 require_relative "../config/environment.rb"
+require_relative "spec_helper.rb"
 
-let (:math24) { Math24.new() }
+
 
 describe 'Running math24' do
+  let (:math24) { Math24.new() }
+
   context 'entering commands' do
     describe 'help' do
       it 'prints instructions on how to use solver' do
         math24.stub(:gets).and_return('help', 'exit')
         help_output = capture_stdout { math24.call }
         expect(help_output).to match(/Input four numbers, and Math24 solution will be printed/)
+      end
+    end
+
+    describe 'exit' do
+      it 'prints a goodbye message' do
+        math24.stub(:gets).and_return('exit')
+        exit_output = capture_stdout { math24.call }
+        expect(exit_output).to match(/Thanks for playing!/)
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
-=begin
+
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
@@ -25,6 +25,7 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
 
+=begin
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
@@ -39,6 +40,7 @@ RSpec.configure do |config|
   # end of the spec run, to help surface which specs are running
   # particularly slow.
   config.profile_examples = 10
+=end
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
@@ -52,6 +54,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 
+=begin
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -75,4 +78,15 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 =end
+end
+
+def capture_stdout(&block)
+  original_stdout = $stdout
+  $stdout = fake = StringIO.new
+  begin
+    yield
+  ensure
+    $stdout = original_stdout
+  end
+  fake.string
 end
