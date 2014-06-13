dcb7260c1f82fd8ecd108c749635d58871a5db10
randallreedjr <randallreedjr@gmail.com>
2014-06-13 11:47:44 -0400

    Make CLI into class, create math24 executable

--- math24cli.rb
+++ math24cli.rb
@@ -1,30 +1,34 @@
 require_relative 'math24.rb'
-
-def run()
-  puts "Welcome to Math 24 solver! Please enter your numbers to see solution"
-  command = ""
-  operators = ["+", "-", "*", "/"]
-  solver = Math24.new(operators)
-  command = gets.chomp
-  while command.downcase != "exit"
-    number_entry = command
-    if number_entry.include?(",")
-      number_entry.gsub!(" ", "")
-      numbers = number_entry.split(",")
-    else
-      number_entry.squeeze(" ")
-      numbers = number_entry.split(" ")
-    end
-    if numbers.size == 4
-      solver.numbers = numbers
-      puts solver.solve()
-    else
-      puts "Please enter exactly four numbers"
-    end
-
-    command = gets.chomp
+class Math24CLI
+  attr_reader :operators
+  
+  def initialize
+    @operators = ["+", "-", "*", "/"]
   end
 
-end
+  def run()
+    puts "Welcome to Math 24 solver! Please enter your numbers to see solution"
+    command = ""
+    solver = Math24.new(operators)
+    command = gets.chomp
+    while command.downcase != "exit"
+      number_entry = command
+      if number_entry.include?(",")
+        number_entry.gsub!(" ", "")
+        numbers = number_entry.split(",")
+      else
+        number_entry.squeeze(" ")
+        numbers = number_entry.split(" ")
+      end
+      if numbers.size == 4
+        solver.numbers = numbers
+        puts solver.solve()
+      else
+        puts "Please enter exactly four numbers"
+      end
 
-run()
+      command = gets.chomp
+    end
+
+  end
+end
