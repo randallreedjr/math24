2e8fe30052b995e4bec1d41b8b30db04cdcbfa2c
randallreedjr <randallreedjr@gmail.com>
2014-06-13 14:17:02 -0400

    Hopefully fix file renames

--- lib/math24cli.rb
+++ lib/math24cli.rb
@@ -11,19 +11,22 @@ class Math24CLI
     solver = Math24.new(operators)
     command = gets.chomp
     while command.downcase != "exit"
-      number_entry = command
-      if number_entry.include?(",")
-        number_entry.gsub!(" ", "")
-        numbers = number_entry.split(",")
+      if command.downcase == "help"
+        puts "Input four numbers, and Math24 solution will be printed"
       else
-        number_entry.squeeze(" ")
-        numbers = number_entry.split(" ")
-      end
-      if numbers.size == 4
-        solver.numbers = numbers
-        puts solver.solve()
-      else
-        puts "Please enter exactly four numbers"
+        number_entry = command
+        if number_entry.include?(",")
+          numbers =number_entry.gsub(" ", "").split(",")
+        else
+          number_entry.squeeze(" ")
+          numbers = number_entry.split(" ")
+        end
+        if numbers.size == 4
+          solver.numbers = numbers
+          puts solver.solve()
+        else
+          puts "Please enter exactly four numbers"
+        end
       end
 
       command = gets.chomp
