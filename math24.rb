6d90a3bc4d8768e4ac72910eb1f9392de3b5638d
randallreedjr <randallreedjr@gmail.com>
2014-06-13 10:59:54 -0400

    Ensures numbers are provided before attempting to solve

--- math24.rb
+++ math24.rb
@@ -8,8 +8,13 @@ class Math24
     @target = 24
   end
 
-  def solve()
-    @numbers = numbers
+  def solve(numbers = [])
+    if numbers.empty?
+      return "Please provide numbers" if @number.empty?
+    else
+      @numbers = numbers
+    end
+    
     index = 1
     op_permutation = @operators.repeated_permutation(3)
     num_permutation = @numbers.permutation(4).to_a.uniq
@@ -24,14 +29,15 @@ class Math24
         if current_result == @target
           if (operators.include?("+") || operators.include?("-")) && (operators.include?("*") || operators.include?("/"))
             #Might need parentheses
-            puts "((#{numbers[0]} #{operators[0]} #{numbers[1]}) #{operators[1]} #{numbers[2]}) #{operators[2]} #{numbers[3]} = 24"
+            return "((#{numbers[0]} #{operators[0]} #{numbers[1]}) #{operators[1]} #{numbers[2]}) #{operators[2]} #{numbers[3]} = 24"
           else
-            puts "#{numbers[0]} #{operators[0]} #{numbers[1]} #{operators[1]} #{numbers[2]} #{operators[2]} #{numbers[3]} = 24"
+            return "#{numbers[0]} #{operators[0]} #{numbers[1]} #{operators[1]} #{numbers[2]} #{operators[2]} #{numbers[3]} = 24"
           end
           break
         end
       end
     end
+    return "No solution found"
   end
 end
 
