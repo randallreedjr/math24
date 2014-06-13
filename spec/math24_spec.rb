8cb44c115755ad3f423730e38899b60feddf27e6
randallreedjr <randallreedjr@gmail.com>
2014-06-13 14:40:21 -0400

    Add test for harder problem

--- spec/math24_spec.rb
+++ spec/math24_spec.rb
@@ -8,39 +8,50 @@ describe Math24, "#initialize" do
 end
 
 describe Math24, "solve" do
-  it "solves a simple addition example" do
-    math24 = Math24.new()
-    math24.numbers = ["6", "6", "6", "6"]
-    expect(math24.solve).to eq("6 + 6 + 6 + 6 = 24")
-  end
-
-  it "provides feedback if no solution found" do
-    math24 = Math24.new()
-    math24.numbers = ["1", "1", "1", "1"]
-    expect(math24.solve).to eq("No solution found")
-  end
-
   it "only accepts integers" do
     math24 = Math24.new()
     math24.numbers = ["1", "1", "1", "1.5"]
     expect(math24.solve).to eq("Please only enter integers between 1 and 9")
   end
 
-  it "only does not accept negative numbers" do
+  it "does not accept negative numbers" do
     math24 = Math24.new()
     math24.numbers = ["1", "1", "1", "-1"]
     expect(math24.solve).to eq("Please only enter integers between 1 and 9")
   end
 
-  it "only does not accept zero" do
+  it "does not accept zero" do
     math24 = Math24.new()
     math24.numbers = ["1", "1", "1", "0"]
     expect(math24.solve).to eq("Please only enter integers between 1 and 9")
   end
 
-  it "only does not accept numbers greater than 9" do
+  it "does not accept numbers greater than 9" do
     math24 = Math24.new()
     math24.numbers = ["1", "1", "1", "10"]
     expect(math24.solve).to eq("Please only enter integers between 1 and 9")
+  end
+
+  it "solves a simple addition example" do
+    math24 = Math24.new()
+    math24.numbers = ["6", "6", "6", "6"]
+    expect(math24.solve).to eq("6 + 6 + 6 + 6 = 24")
+  end
+
+  it "solves a more complex example" do
+    math24 = Math24.new()
+    math24.numbers = ["9", "8", "3", "2"]
+    expect(math24.solve).to eq("((9 - 3) * 8) / 2 = 24")
+  end
+
+  it "accepts numbers as arguments" do
+    math24 = Math24.new()
+    expect(math24.solve(["9","8","3","2"])).to eq("((9 - 3) * 8) / 2 = 24")
+  end
+
+  it "provides feedback if no solution found" do
+    math24 = Math24.new()
+    math24.numbers = ["1", "1", "1", "1"]
+    expect(math24.solve).to eq("No solution found")
   end
 end
