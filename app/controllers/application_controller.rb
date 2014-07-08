7bada82545e57be88f7e9fca51ccc902ecdaa346
randallreedjr <randallreedjr@gmail.com>
2014-07-08 10:53:52 -0400

    Add sqlite3 gem 1.3.9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < Sinatra::Base
-  #register Sinatra::ActiveRecordExtension
+  register Sinatra::ActiveRecordExtension
   set :static, true
   set :views, Proc.new { File.join(root, "../views/") }
 end
