a9d8c0221b054339850a3b38d534e6100cf1434b
randallreedjr <randallreedjr@gmail.com>
2014-07-08 10:56:35 -0400

    Comment out database stuff

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,9 @@ require_relative "../lib/math24solvercli.rb"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/sinatra_pigeon#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/math24#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
 require_all 'app'
