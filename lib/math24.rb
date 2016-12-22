1597f2da90bed2bd9a2183505c538c15ffc81be0
Randall Reed, Jr <randallreedjr@gmail.com>
2016-12-21 22:32:58 -0500

    Add ability to solve a problem

--- lib/math24.rb
+++ lib/math24.rb
@@ -1,17 +1,57 @@
 require "math24/version"
 
 module Math24
+  OPERATORS = ['+','-','*','/']
   def self.generate_problem
     loop do
-      return Array.new(4) { rand(1...9) }
-      # TODO: Verify solution exists
-
-      # math24 = Math24Solver.new()
-      # math24.numbers = numbers
-      # if math24.solve() != "No solution found"
-      #   @numbers = numbers
-      #   return numbers
-      # end
+      problem = Array.new(4) { rand(1...9) }
+      return problem if solve(problem)
     end
+  end
+
+  def self.solve(problem)
+    raise ArgumentError unless /\A\d{4}\z/.match problem.join
+
+    op_permutation = OPERATORS.repeated_permutation(3)
+    # Unique permutations removes duplicates when a number appears multiple
+    # times in the problem
+    num_permutation = problem.permutation(4).to_a.uniq
+
+    num_permutation.each do |numbers|
+      op_permutation.each do |operators|
+        begin
+          forward_result = instance_eval("((#{numbers[0].to_f} #{operators[0]} #{numbers[1]}.to_f) #{operators[1]} #{numbers[2]}.to_f) #{operators[2]} #{numbers[3].to_f}")
+        rescue ZeroDivisionError
+          forward_result = 0
+        end
+
+        begin
+          alternate_result = instance_eval("(#{numbers[0].to_f} #{operators[0]} #{numbers[1].to_f}) #{operators[1]} (#{numbers[2].to_f} #{operators[2]} #{numbers[3].to_f})")
+        rescue ZeroDivisionError
+          alternate_result = 0
+        end
+
+        begin
+          reverse_result = instance_eval("#{numbers[0].to_f} #{operators[0]} (#{numbers[1].to_f} #{operators[1]} (#{numbers[2].to_f} #{operators[2]} #{numbers[3].to_f}))")
+        rescue ZeroDivisionError
+          reverse_result = 0
+        end
+
+        if forward_result == 24
+          if (operators.include?("+") || operators.include?("-")) && (operators.include?("*") || operators.include?("/"))
+            #Might need parentheses for order of operations
+            return "((#{numbers[0]} #{operators[0]} #{numbers[1]}) #{operators[1]} #{numbers[2]}) #{operators[2]} #{numbers[3]}"
+          else
+            return "#{numbers[0]} #{operators[0]} #{numbers[1]} #{operators[1]} #{numbers[2]} #{operators[2]} #{numbers[3]}"
+          end
+        elsif alternate_result == 24
+          return "(#{numbers[0]} #{operators[0]} #{numbers[1]}) #{operators[1]} (#{numbers[2]} #{operators[2]} #{numbers[3]})"
+        elsif reverse_result == 24
+          return "#{numbers[0]} #{operators[0]} (#{numbers[1]} #{operators[1]} (#{numbers[2]} #{operators[2]} #{numbers[3]}))"
+        end
+      end
+    end
+
+    return false
   end
 end
--- spec/math24_spec.rb
+++ spec/math24_spec.rb
@@ -19,4 +19,45 @@ describe Math24 do
      expect(problem1).not_to eq(problem2)
    end
   end
+
+  describe '#solve' do
+    it 'raises an argument error if parameter is not provided' do
+      begin
+        Math24.solve
+      rescue ArgumentError
+        exception_raised = true
+      end
+
+      expect(exception_raised).to be true
+    end
+
+    it 'raises an argument error if problem array is not provided' do
+      begin
+        Math24.solve []
+      rescue ArgumentError
+        exception_raised = true
+      end
+
+      expect(exception_raised).to be true
+    end
+
+    it 'returns solution if problem is solvable' do
+      solution = Math24.solve [1, 2, 3, 4]
+
+      expect(solution).to_not be_nil
+    end
+
+    it 'returns a valid solution to the problem' do
+      solution = Math24.solve [1, 2, 3, 4]
+      result = instance_eval(solution)
+
+      expect(result).to eq 24
+    end
+
+    it 'returns false if no solution exists' do
+      solution = Math24.solve [1, 1, 1, 1]
+
+      expect(solution).to be false
+    end
+  end
 end
