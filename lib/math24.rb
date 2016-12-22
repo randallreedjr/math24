e795d8b6da19c650c76d9e5732380b96a603c3c1
Randall Reed, Jr <randallreedjr@gmail.com>
2016-12-21 22:11:59 -0500

    Generate a new problem every time

--- lib/math24.rb
+++ lib/math24.rb
@@ -2,12 +2,8 @@ require "math24/version"
 
 module Math24
   def self.generate_problem
-    [1,2,3,4]
-  end
-
-  def generate_problem()
     loop do
-      Array.new(4) { rand(1...9) }
+      return Array.new(4) { rand(1...9) }
       # TODO: Verify solution exists
 
       # math24 = Math24Solver.new()
--- spec/math24_spec.rb
+++ spec/math24_spec.rb
@@ -11,5 +11,12 @@ describe Math24 do
       match = /\A\d{4}\z/.match problem.join
       expect(match).to_not be_nil
     end
+
+    it "generates a different problem each time" do
+     problem1 = Math24.generate_problem
+     problem2 = Math24.generate_problem
+
+     expect(problem1).not_to eq(problem2)
+   end
   end
 end
